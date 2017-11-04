ffe2d15d638b59204165f3a11bf29f0e9ba44ff0
zann10thousand <32677985+zann10thousand@users.noreply.github.com>
2017-11-03 21:20:20 -0500

    11-3-17
    Spawning and Sprites

---
+++ Plaidformer/Assets/AIBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBehavior : MonoBehaviour {
+
+    private Rigidbody2D RB;
+
+	// Use this for initialization
+	void Start () {
+        RB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
