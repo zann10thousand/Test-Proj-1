578e7448841de53b9e7ea031470bd000ab45a758
zann10thousand <32677985+zann10thousand@users.noreply.github.com>
2017-11-21 16:25:40 -0600

    11-21-17 Spawning and Enimy AI

--- Plaidformer/Assets/AIBehavior.cs
+++ Plaidformer/Assets/AIBehavior.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class AIBehavior : MonoBehaviour {
 
     private Rigidbody2D RB;
+    private Collider2D col;
+    private bool edge = false;
+    private int xspeed;
+    public static int range = 10;
+    Vector2 velocity;
 
 	// Use this for initialization
 	void Start () {
         RB = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+        xspeed = Random.Range(-range, range);
+        RB.velocity = new Vector2(xspeed, 0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    // Update is called once per frame
+    void Update () {
+        if (col.gameObject.name == "floor" || col.gameObject.name == "floor(Clone)")
+            edge = false;
+        else
+            edge = true;
+
+        if (edge)
+            RB.velocity = new Vector2(-(RB.velocity.x), 0);
 		
 	}
 }
--- Plaidformer/Assets/Spawning.cs
+++ Plaidformer/Assets/Spawning.cs
@@ -6,6 +6,7 @@ public class Spawning : MonoBehaviour {
 
     public GameObject Floor;
     public GameObject Coin;
+    public GameObject Enemy;
 
     // Use this for initialization
     void Start () {
@@ -22,6 +23,13 @@ public class Spawning : MonoBehaviour {
         Instantiate(Coin, new Vector3(0, 8, 0), Quaternion.identity);
         Instantiate(Coin, new Vector3(15, 12, 0), Quaternion.identity);
         Instantiate(Coin, new Vector3(-15, 12, 0), Quaternion.identity);
+
+        // Creates some eminys
+        Instantiate(Enemy, new Vector3(10, 5, 0), Quaternion.identity);
+        Instantiate(Enemy, new Vector3(-10, 5, 0), Quaternion.identity);
+        Instantiate(Enemy, new Vector3(0, 8, 0), Quaternion.identity);
+        Instantiate(Enemy, new Vector3(15, 12, 0), Quaternion.identity);
+        Instantiate(Enemy, new Vector3(-15, 12, 0), Quaternion.identity);
     }
 	
 	// Update is called once per frame
